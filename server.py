8f7cae826a72479e8c81f7c7b8865cd256da8e49
Nikolas Schuster <ngpnickschuster@gmail.com>
2019-11-29 16:41:33 -0800

    Basic server is finished.

--- server.py
+++ server.py
@@ -1,19 +1,30 @@
 import asyncio
+import time
 
-
-ADDR = 'localhost'
+ADDR = '192.168.0.64'
 PORT = 12345
 CLIENTS = []
+TYPE = 'H'
 
-def serve(reader, writer):
+async def serve(reader, writer):
+	global TYPE
+	global CLIENTS
 	client = writer.get_extra_info('peername')
 	CLIENTS.append(client)
-	if len(CLIENTS) >= 2:
-		print(CLIENTS[0], CLIENTS[1])
-		del CLIENTS[0]
-		del CLIENTS[0]
+	if TYPE == 'C':
+		writer.write(TYPE.encode('utf-8'))
+		writer.write(CLIENTS[index-1][0].encode('utf-8'))
+		TYPE = 'H'
+	else:
+		writer.write(TYPE.encode('utf-8'))
+		TYPE = 'C'
+	
+	if len(CLIENTS) == 2:
+		CLIENTS = []
 
-
+	await writer.drain()
+	writer.close()
+	await writer.wait_closed()
 
 async def main():
     server = await asyncio.start_server(serve, ADDR, PORT)
